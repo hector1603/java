b21e2cef04978d06726d8e1fda70ba894d7e40a1
Hector Ajumado <hector.ajumado@gmail.com>
2024-03-13 11:54:00 -0400

    Crear la clase Cuadrado con sus metodos e instanciarlos

---
+++ Clases/src/com/krakedev/TestCuadrado.java
@@ -0,0 +1,34 @@
+package com.krakedev;
+
+public class TestCuadrado {
+
+	public static void main(String[] args) {
+		Cuadrado cuadradoPequenio = new Cuadrado();
+		Cuadrado cuadradoMediano = new Cuadrado();
+		Cuadrado cuadradoGrande = new Cuadrado();
+		
+		cuadradoPequenio.lado = 3;
+		cuadradoMediano.lado = 5;
+		cuadradoGrande.lado = 8;
+		
+		double areaP = cuadradoPequenio.calcularArea();
+		double perimetroP = cuadradoPequenio.calcularPerimetro();
+		System.out.println("----------------------");
+		System.out.println("Cuadrado pequeño \n Area: " + areaP);
+		System.out.println(" Perimetro: " + perimetroP);
+		
+		double areaM = cuadradoMediano.calcularArea();
+		double perimetroM = cuadradoMediano.calcularPerimetro();
+		System.out.println("----------------------");
+		System.out.println("Cuadrado mediano \n Area: " + areaM);
+		System.out.println(" Perimetro: " + perimetroM);
+		
+		double areaG = cuadradoGrande.calcularArea();
+		double perimetroG = cuadradoGrande.calcularPerimetro();
+		System.out.println("----------------------");
+		System.out.println("Cuadrado grande \n Area: " + areaG);
+		System.out.println(" Perimetro: " + perimetroG);
+
+	}
+
+}
